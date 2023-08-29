9d717710bde642077e18da4dfdd5d2642171bdb3
Bibek Kumar Sah <84366074+Shahbibek@users.noreply.github.com>
2023-08-30 00:41:19 +0530

    Update by bibek try.sql
    Update by bibek

--- try.sql
+++ try.sql
@@ -1,7 +1,13 @@
+USE [try_me]
+GO
+/****** Object:  StoredProcedure [dbo].[DATA_MASKING_COLUMN_LEVEL]    Script Date: 29/08/2023 23:32:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
 ALTER PROCEDURE [dbo].[DATA_MASKING_COLUMN_LEVEL]
 (
-	@database_name NVARCHAR(MAX),
     @table_name NVARCHAR(MAX),
     @json_input NVARCHAR(MAX)
 )
@@ -12,57 +18,45 @@ BEGIN
     DECLARE @sql NVARCHAR(MAX)
     DECLARE @column_name NVARCHAR(MAX)
     DECLARE @masking_function NVARCHAR(MAX)
-    DECLARE @is_nullable BIT
 
 
 	CREATE TABLE #TempTable (
     column_name NVARCHAR(MAX),
-    masking_function NVARCHAR(MAX),
-    is_nullable BIT
+    masking_function NVARCHAR(MAX)    
 	)
 
     -- Parse the JSON input
-    INSERT INTO #TempTable (column_name, masking_function, is_nullable)
-    SELECT column_name, masking_function, is_nullable
+    INSERT INTO #TempTable (column_name, masking_function)
+    SELECT column_name, masking_function
     FROM OPENJSON(@json_input)
     WITH (
         column_name NVARCHAR(MAX) '$.column_name',
-        masking_function NVARCHAR(MAX) '$.masking_function',
-        is_nullable BIT '$.is_nullable'
+        masking_function NVARCHAR(MAX) '$.masking_function'     
     );
 
     -- Cursor to iterate through columns
     DECLARE @column_cursor CURSOR
     SET @column_cursor = CURSOR FOR
-    SELECT column_name, masking_function, is_nullable
+    SELECT column_name, masking_function
     FROM #TempTable
 
     OPEN @column_cursor
 
-    FETCH NEXT FROM @column_cursor INTO @column_name, @masking_function, @is_nullable
+    FETCH NEXT FROM @column_cursor INTO @column_name, @masking_function
 
     -- Loop through columns
     WHILE @@FETCH_STATUS = 0
     BEGIN
+				
         -- Construct the dynamic SQL statement based on parameters
-        SET @sql = 'ALTER TABLE ' + @database_name + '.' + @table_name +
-                   ' ALTER COLUMN ' + @column_name + ' ADD MASKED WITH (FUNCTION = ' + @masking_function;
+        SET @sql = 'ALTER TABLE ' + QUOTENAME(@table_name) +
+                   ' ALTER COLUMN ' + QUOTENAME(@column_name) + ' ADD MASKED WITH (FUNCTION = '+ QUOTENAME(@masking_function, '''') + ')'; 
 
-        -- Check if column is nullable and adjust the SQL query accordingly
-        IF @is_nullable = 1
-			BEGIN
-				SET @sql = @sql  + ') NULL';
-			END
-		ELSE
-			BEGIN
-				SET @sql = @sql  + ') NOT NULL';
-			END
-
-
-        -- Execute the dynamic SQL
+		print(@sql);
+				           
         EXEC sp_executesql @sql
 
-        FETCH NEXT FROM @column_cursor INTO @column_name, @masking_function, @is_nullable
+        FETCH NEXT FROM @column_cursor INTO @column_name, @masking_function
     END
 
     CLOSE @column_cursor
@@ -70,30 +64,9 @@ BEGIN
 END
 
 
-CREATE USER TestUser WITHOUT LOGIN;
-
-EXECUTE AS USER = 'TestUser';
-
-GRANT SELECT TO TestUser;
-
-SELECT * FROM TBLEMPMST;
-
-USE [try_me]
-GO
-
--- tried
-
-DECLARE	@return_value int
-
-EXEC	@return_value = [dbo].[DATA_MASKING_COLUMN_LEVEL]
-		@database_name = N'try_me',
+EXECUTE	[dbo].[DATA_MASKING_COLUMN_LEVEL]
 		@table_name = N'TBLEMPMST',
-		@json_input = N'[{"column_name": "Nam", "masking_function": "default", "is_nullable": 1}, {"column_name": "Age", "masking_function": "default", "is_nullable": 0}]'
+		@json_input = N'[{"column_name": "Name", "masking_function": "default()"}, {"column_name": "Age", "masking_function": "default()"}]'
 
-SELECT	'Return Value' = @return_value
-
-GO
 
 ALTER DATABASE try_me SET COMPATIBILITY_LEVEL = 140;
-
-
